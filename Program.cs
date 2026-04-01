ffc5f5f74a0ca79b06d71283ee5d6b490f702938
1 <1@PC-A417-07>
2026-04-01 18:14:21 +0300

    Add *.cs-files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BorisovNA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // Борисов НикитаАлексеевич Ам-1-25
+
+       
+        static public double Sqr(double X)
+        {
+            return X * X;
+        }
+    }
+}
